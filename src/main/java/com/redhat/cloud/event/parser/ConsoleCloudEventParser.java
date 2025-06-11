d0c1e53afe136789086d7d03f6d3bd4f092a48aa
Guillaume Duval <117720964+g-duval@users.noreply.github.com>
2025-06-11 10:08:55 +0200

    [RHCLOUD-40531] Bump json-schema-validator dependency by going through breaking changes (#314)
    * [RHCLOUD-40531] Bump json-schema-validator dependency by going through breaking changes
    
    * Update LocalDateTimeValidator

--- src/main/java/com/redhat/cloud/event/parser/ConsoleCloudEventParser.java
+++ src/main/java/com/redhat/cloud/event/parser/ConsoleCloudEventParser.java
@@ -4,18 +4,20 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.networknt.schema.AbsoluteIri;
 import com.networknt.schema.ApplyDefaultsStrategy;
+import com.networknt.schema.Formats;
 import com.networknt.schema.JsonMetaSchema;
 import com.networknt.schema.JsonSchema;
 import com.networknt.schema.JsonSchemaException;
 import com.networknt.schema.JsonSchemaFactory;
 import com.networknt.schema.NonValidationKeyword;
+import com.networknt.schema.PathType;
 import com.networknt.schema.SchemaValidatorsConfig;
 import com.networknt.schema.SpecVersion;
 import com.networknt.schema.ValidationResult;
 import com.networknt.schema.ValidatorTypeCode;
-import com.networknt.schema.uri.URIFactory;
-import com.networknt.schema.uri.URLFactory;
+import com.networknt.schema.resource.SchemaMapper;
 import com.redhat.cloud.event.core.v1.RHELSystem;
 import com.redhat.cloud.event.parser.exceptions.ConsoleCloudEventParsingException;
 import com.redhat.cloud.event.parser.exceptions.ConsoleCloudEventValidationException;
@@ -25,39 +27,35 @@ import com.redhat.cloud.event.parser.validators.LocalDateTimeValidator;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URI;
 import java.util.List;
 
 public class ConsoleCloudEventParser {
 
     private static final String schemaPath = "/schemas/events/v1/events.json";
 
-    static class CloudEventURIFactory implements URIFactory {
+    static class CustomSchemaMapper implements SchemaMapper {
 
         private static final String baseUrl = "https://console.redhat.com/api";
-        private final URLFactory urlFactory = new URLFactory();
         private final String base;
 
-        CloudEventURIFactory() {
+        CustomSchemaMapper() {
             String fullPath = RHELSystem.class.getResource(schemaPath).toString();
             base = fullPath.substring(0, fullPath.length() - schemaPath.length());
         }
 
         @Override
-        public URI create(String uri) {
-            return urlFactory.create(replaceBase(uri));
-        }
-
-        @Override
-        public URI create(URI baseURI, String segment) {
-            return urlFactory.create(URI.create(replaceBase(baseURI.toString())), segment);
+        public AbsoluteIri map(AbsoluteIri absoluteIRI) {
+            String iri = absoluteIRI.toString();
+            if (iri.startsWith(baseUrl)) {
+                return AbsoluteIri.of(replaceBase(iri));
+            }
+            return null;
         }
 
         private String replaceBase(String uri) {
             if (uri.startsWith(baseUrl)) {
-                uri = base + uri.substring(baseUrl.length(), uri.length() - 1);
+                uri = base + uri.substring(baseUrl.length());
             }
-
             return uri;
         }
     }
@@ -128,12 +126,18 @@ public class ConsoleCloudEventParser {
     }
 
     private JsonSchema getJsonSchema() {
-        SchemaValidatorsConfig schemaValidatorsConfig = new SchemaValidatorsConfig();
-        schemaValidatorsConfig.setApplyDefaultsStrategy(new ApplyDefaultsStrategy(
-                true,
-                true,
-                true
-        ));
+        SchemaValidatorsConfig schemaValidatorsConfig = SchemaValidatorsConfig.builder()
+            .pathType(PathType.LEGACY)
+            .errorMessageKeyword("message")
+            .nullableKeywordEnabled(true)
+            .applyDefaultsStrategy(
+                new ApplyDefaultsStrategy(
+            true,
+            true,
+            true
+                ))
+            .build();
+
 
         try (InputStream jsonSchemaStream = RHELSystem.class.getResourceAsStream(schemaPath)) {
             JsonNode schema = objectMapper.readTree(jsonSchemaStream);
@@ -158,10 +162,10 @@ public class ConsoleCloudEventParser {
     private static JsonSchemaFactory jsonSchemaFactory() {
         String ID = "$id";
 
-        JsonMetaSchema overrideDateTimeValidator = new JsonMetaSchema.Builder(JsonMetaSchema.getV7().getUri())
+        JsonMetaSchema overrideDateTimeValidator = new JsonMetaSchema.Builder(JsonMetaSchema.getV7().getIri())
                 .idKeyword(ID)
-                .addKeywords(ValidatorTypeCode.getNonFormatKeywords(SpecVersion.VersionFlag.V7))
-                .addKeywords(List.of(
+                .keywords(ValidatorTypeCode.getKeywords(SpecVersion.VersionFlag.V7))
+                .keywords(List.of(
                         new NonValidationKeyword("examples"),
                         new NonValidationKeyword("$schema"),
                         new NonValidationKeyword("definitions"),
@@ -170,13 +174,14 @@ public class ConsoleCloudEventParser {
                         new NonValidationKeyword("description"),
                         new NonValidationKeyword("contentEncoding")
                 ))
-                .addFormats(JsonMetaSchema.COMMON_BUILTIN_FORMATS)
-                .addFormat(new LocalDateTimeValidator())
+                .formats(Formats.DEFAULT)
+                .format(new LocalDateTimeValidator())
+                .specification(SpecVersion.VersionFlag.V7)
                 .build();
 
-        return new JsonSchemaFactory.Builder().defaultMetaSchemaURI(overrideDateTimeValidator.getUri())
-                .addMetaSchema(overrideDateTimeValidator)
-                .uriFactory(new CloudEventURIFactory(), "https")
+        return new JsonSchemaFactory.Builder().defaultMetaSchemaIri(overrideDateTimeValidator.getIri())
+                .metaSchema(overrideDateTimeValidator)
+                .schemaMappers(schemaMappers -> schemaMappers.add(new CustomSchemaMapper()))
                 .build();
 
     }
